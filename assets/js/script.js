865129b80e1fd89ea71971677d9111e03232fb71
Jessica Oboh <jessicarichardson@Jessicas-MBP.lan>
2022-10-24 21:31:08 -0700

    basic html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 var key = '34a134b6f5c8f05caaa647f4e9e2c70e';
-//var url = 'https://api.themoviedb.org/3/trending/movie/day?api_key=' +key;
-var url ='https://api.themoviedb.org/3/genre/movie/list?api_key=' + key;
+var url = 'https://api.themoviedb.org/3/trending/movie/day?api_key=' +key;
+//var url ='https://api.themoviedb.org/3/genre/movie/list?api_key=' + key;
 //https://api.themoviedb.org/3/genre/movie/list?api_key=34a134b6f5c8f05caaa647f4e9e2c70e&language=en-US
 console.log(url);
 
