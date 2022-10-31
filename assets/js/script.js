958dbf72d52d6a4a0c9d900ca8e2d0489f6840f5
Supapun Wichachuai <Supapun.Wichachuai@Microchip.com>
2022-10-30 19:22:46 -0700

    update movie functions in js and html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,35 @@
 var movieKey = '34a134b6f5c8f05caaa647f4e9e2c70e';
+
+var divMovie = $("#random-movie");
+var coctail = $(".random-cocktail");
 // API: Trending movies
 var movieUrl = 'https://api.themoviedb.org/3/trending/movie/day?api_key=' + movieKey;
-var movie = $(".random-movie");
-var coctail = $(".random-cocktail");
 
 // Get random movie
-function randomMovie() {
-    fetch(movieUrl)
+function randomMovie(url) {
+    fetch(url)
         .then(function (resp) {
             return resp.json()
         })
         .then(function (data) {
-            var rndmMovie = data.results;
+            var rndmMovie = data.results;         
             var rndmMovieTitleIndex = Math.floor(Math.random() * rndmMovie.length);
             var rndmMovieTitle = rndmMovie[rndmMovieTitleIndex].title;
 
-            console.log(rndmMovie[rndmMovieTitleIndex]);
-            $('#movieTitle').append("<strong>" + rndmMovieTitle);
-            $('#movieYear').append(rndmMovie[rndmMovieTitleIndex].release_date);
-            $('#movieOverview').append(rndmMovie[rndmMovieTitleIndex].overview);
-
-            movieGenre(rndmMovie[rndmMovieTitleIndex].genre_ids);           
+            divMovie.empty();
+            divMovie.append("<p>Title: <strong>" + rndmMovieTitle);
+            divMovie.append("<p>Release Date: " + rndmMovie[rndmMovieTitleIndex].release_date);
+            movieGenre(rndmMovie[rndmMovieTitleIndex].genre_ids);
+            divMovie.append("<p>Overview: " + rndmMovie[rndmMovieTitleIndex].overview);            
         })
         .catch(function () {
             // catch any errors
         });
 }
-randomMovie();
-
+window.onload = randomMovie(movieUrl);
 
 function movieGenre(genreId) {
-    console.log(genreId)
+
     var movieGenreURL = "https://api.themoviedb.org/3/genre/movie/list?api_key=" + movieKey + "&language=en-US";
     fetch(movieGenreURL)
         .then(function (resp) {
@@ -42,17 +41,24 @@ function movieGenre(genreId) {
             for (var i = 0; i < genreId.length; i++) {
                 for (var k = 0; k < genres.length; k++) {
                     if (genres[k].id == genreId[i]) {
-                        console.log(genres[k].name);
-                        strGenres += ","+ genres[k].name;
+                        strGenres += "," + genres[k].name;
                     }
                 }
             }
-            $('#movieGenre').append(strGenres.slice(1));
+            divMovie.append("<p>Genre: " + strGenres.slice(1));
         })
         .catch(function () {
             // catch any errors
         });
 }
+
+$("#genre-submit").on('click', function () {
+    var selectedGenre = $("#genre-selector option:selected").val();
+    // API: Discovery movies
+    var movieByGenreUrl = "https://api.themoviedb.org/3/discover/movie?api_key=" + movieKey + "&language=en-US&sort_by=popularity.desc&with_genres=" + selectedGenre+"&with_watch_monetization_types=flatrate&include_adult=false&include_video=false&page=1";
+    randomMovie(movieByGenreUrl);
+});
+
 
 // Get random Cocktail with intructions using API random cocktail
 var drinkUrl = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
